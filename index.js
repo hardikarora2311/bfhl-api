0e6c9bcc7cd4b14bf54c22cd3e6d422e8744b70b
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-09-08 16:00:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,62 +1,75 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-// POST method endpoint
-app.post('/bfhl', (req, res) => {
-  try {
-    const requestData = req.body.data;
-
-    // Input validation
-    if (!Array.isArray(requestData)) {
-      return res.status(400).json({ error: 'Invalid input. Expected an array.' });
-    }
-
-    const numbers = [];
-    const alphabets = [];
-
-    // Process the requestData to separate numbers and alphabets
-    for (const item of requestData) {
-      if (typeof item === 'number') {
-        numbers.push(item.toString());
-      } else if (typeof item === 'string' && item.length === 1 && /^[A-Za-z]$/.test(item)) {
-        alphabets.push(item.toUpperCase()); // Convert to uppercase for case-insensitive comparison
-      }
-    }
-
-    // Find the highest alphabet
-    const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
-
-    // Construct the response
-    const response = {
-      is_success: true,
-      user_id: 'hardik_arora_23112002',
-      email: 'ha3029@srmist.edu.in',
-      roll_number: 'RA2011003010419',
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  console.log(req.path, req.method);
+  next();
+});
+function findHighestAlphabet(alphabets) {
+  if (!alphabets || alphabets.length === 0) {
+    return [];
+  }
+  return [alphabets.reduce((a, b) => (a > b ? a : b))];
+}
+app.use((error, req, res, next) => {
+  const numbers = [];
+  const alphabets = [];
+  const highest_alphabet = [];
+  if (error instanceof SyntaxError && error.status === 400 && "body" in error) {
+    res.status(400).json({
+      is_success: false,
+      user_id: "hardik_arora_23112002",
+      email: "ha3029@srmist.edu.in",
+      roll_number: "RA2011003010419",
       numbers,
       alphabets,
       highest_alphabet,
-    };
-
-    res.status(200).json(response);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    });
+  } else {
+    next();
   }
 });
 
-// GET method endpoint
-app.get('/bfhl', (req, res) => {
-  // This is a hardcoded response for the GET request
-  res.status(200).json({ operation_code: 1 });
+app.get("/bfhl", (req, res) => {
+  res.json({ operation_code: 1 });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/bfhl", async (req, res) => {
+  let jsonData = req.body.data;
+  const numbers = [];
+  const alphabets = [];
+
+  jsonData.forEach((element) => {
+    if (typeof element === "number") {
+      numbers.push(element);
+    } else if (typeof element === "string") {
+      alphabets.push(element);
+    }
+  });
+  const highest_alphabet = findHighestAlphabet(alphabets);
+
+  res.json({
+    is_success: true,
+    user_id: "hardik_arora_23112002",
+    email: "ha3029@srmist.edu.in",
+    roll_number: "RA2011003010419",
+    numbers,
+    alphabets,
+    highest_alphabet,
+  });
+});
+
+app.listen(() => {
+  console.log("Server Up and Running");
 });
