ecd3727530be484cf4eacbd93c4375121e1ff92c
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-09-08 15:06:36 +0530

    post method

--- server.js
+++ server.js
@@ -10,28 +10,53 @@ app.use(cors());
 
 // POST method endpoint
 app.post('/bfhl', (req, res) => {
-  // Your logic for handling POST requests goes here
-  const requestData = req.body.data;
-  
-  // Process the requestData and construct the response
-  const response = {
-    is_success: true,
-    user_id: "hardik_arora_23112001",
-    email: "ha3029@srmist.edu.in",
-    roll_number: "RA2011003010419",
-    numbers: requestData.filter(item => !isNaN(item)),
-    alphabets: requestData.filter(item => typeof item === 'string' && item.length === 1),
-    highest_alphabet: [requestData.filter(item => typeof item === 'string' && item.length === 1).sort().pop()]
-  };
+  try {
+    const requestData = req.body.data;
 
-  res.json(response);
+    // Input validation
+    if (!Array.isArray(requestData)) {
+      return res.status(400).json({ error: 'Invalid input. Expected an array.' });
+    }
+
+    const numbers = [];
+    const alphabets = [];
+
+    // Process the requestData to separate numbers and alphabets
+    for (const item of requestData) {
+      if (typeof item === 'number') {
+        numbers.push(item.toString());
+      } else if (typeof item === 'string' && item.length === 1 && /^[A-Za-z]$/.test(item)) {
+        alphabets.push(item.toUpperCase()); // Convert to uppercase for case-insensitive comparison
+      }
+    }
+
+    // Find the highest alphabet
+    const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
+
+    // Construct the response
+    const response = {
+      is_success: true,
+      user_id: 'john_doe_17091999',
+      email: 'john@xyz.com',
+      roll_number: 'ABCD123',
+      numbers,
+      alphabets,
+      highest_alphabet,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // GET method endpoint
 app.get('/bfhl', (req, res) => {
+  // This is a hardcoded response for the GET request
   res.status(200).json({ operation_code: 1 });
 });
 
 app.listen(port, () => {
-  console.log(`Server is up and running!`);
+  console.log(`Server is running on port ${port}`);
 });
