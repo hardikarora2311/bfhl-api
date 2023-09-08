4aebcdf6b084b1ddb01d18527ccac67444144bd2
hardikarora2311 <70485045+hardikarora2311@users.noreply.github.com>
2023-09-08 15:23:09 +0530

    update

--- index.js
+++ index.js
@@ -1,64 +1,62 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-app.use(cors());
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  console.log(req.path, req.method);
-  next();
-});
-app.use((error, req, res, next) => {
-  const numbers = [];
-  const alphabets = [];
-  if (error instanceof SyntaxError && error.status === 400 && "body" in error) {
-    res.status(400).json({
-      is_success: false,
-      user_id: "hardik_arora_23112002",
-      email: "ha3029@srmist.edu.in",
-      roll_number: "RA2011003010419",
+app.use(cors());
+
+// POST method endpoint
+app.post('/bfhl', (req, res) => {
+  try {
+    const requestData = req.body.data;
+
+    // Input validation
+    if (!Array.isArray(requestData)) {
+      return res.status(400).json({ error: 'Invalid input. Expected an array.' });
+    }
+
+    const numbers = [];
+    const alphabets = [];
+
+    // Process the requestData to separate numbers and alphabets
+    for (const item of requestData) {
+      if (typeof item === 'number') {
+        numbers.push(item.toString());
+      } else if (typeof item === 'string' && item.length === 1 && /^[A-Za-z]$/.test(item)) {
+        alphabets.push(item.toUpperCase()); // Convert to uppercase for case-insensitive comparison
+      }
+    }
+
+    // Find the highest alphabet
+    const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
+
+    // Construct the response
+    const response = {
+      is_success: true,
+      user_id: 'john_doe_17091999',
+      email: 'john@xyz.com',
+      roll_number: 'ABCD123',
       numbers,
       alphabets,
-    });
-  } else {
-    next();
+      highest_alphabet,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.get("/bfhl", (req, res) => {
-  res.json({ operation_code: 1 });
+// GET method endpoint
+app.get('/bfhl', (req, res) => {
+  // This is a hardcoded response for the GET request
+  res.status(200).json({ operation_code: 1 });
 });
 
-app.post("/bfhl", async (req, res) => {
-  let jsonData = req.body.data;
-  const numbers = [];
-  const alphabets = [];
-
-  jsonData.forEach((element) => {
-    if (typeof element === "number") {
-      numbers.push(element);
-    } else if (typeof element === "string") {
-      alphabets.push(element);
-    }
-  });
-
-  res.json({
-    is_success: true,
-    user_id: "hardik_arora_23112002",
-    email: "ha3029@srmist.edu.in",
-    roll_number: "RA2011003010419",
-    numbers,
-    alphabets,
-  });
-});
-
-app.listen(process.env.PORT, () => {
-  console.log("Server Up and Running");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
